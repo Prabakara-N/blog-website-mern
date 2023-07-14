415d68fdf8205f9f22d10d6810a1a1b33731c984
Prabakaran-M <mspk028@gmail.com>
2023-07-14 20:32:09 +0530

    .

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: false },
-  googleId: { type: String, required: false },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  imageFile: String,
+  bio: { type: String, required: true },
   id: { type: String },
 });
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,16 @@
 import express from "express";
 const router = express.Router();
 
-import { signup, signin } from "../controllers/user.js";
+import {
+  signup,
+  signin,
+  addUserInfo,
+  getUserInfo,
+} from "../controllers/user.js";
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.patch("/userinfo/:id", addUserInfo);
+router.get("/userinfo/:id", getUserInfo);
 
 export default router;
